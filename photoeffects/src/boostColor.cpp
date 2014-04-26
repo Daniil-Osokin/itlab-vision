81e1f176ca7180e450fb1d7f74964dbe44f0a63c
unknown <skrebkov.a@ws-2k-114-06n.ccam.cmc.unn.net>
2014-04-27 00:37:37 +0400

    add timers

--- photoeffects/src/boostColor.cpp
+++ photoeffects/src/boostColor.cpp
@@ -1,8 +1,57 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/internal.hpp>
 #include "photoeffects.hpp"
+
+#include <stdio.h>
+#define TIMER_START(name) int64 t_##name = getTickCount()
+#define TIMER_END(name) printf("TIMER_" #name ":\t%6.2fms\n", \
+            1000.f * ((getTickCount() - t_##name) / getTickFrequency()))
 
 using namespace cv;
 
-int boostColor(cv::InputArray src, cv::OutputArray dst, float intensity)
+namespace
+{
+class BoostColorInvoker
+{
+public:
+    BoostColorInvoker(const Mat& src, Mat& dst, float intensity)
+        : src_(src),
+          dst_(dst),
+          intensity_(intensity),
+          height_(src.cols) {}
+
+    void operator()(const BlockedRange& rows) const
+    {
+        Mat srcStripe = src_.rowRange(rows.begin(), rows.end());
+        srcStripe /= 255.0f;
+        cvtColor(srcStripe, srcStripe, CV_BGR2HLS);
+        int stripeWidth = srcStripe.rows;
+		printf("%d\n", stripeWidth);
+		printf("%d\n", getNumThreads());
+        for (int y = 0; y < stripeWidth; y++)
+        {
+            float* row = (float*)srcStripe.row(y).data;
+            for (int x = 0; x < height_*3; x += 3)
+            {
+                row[x + 2] = min(row[x + 2] + intensity_, 1.0f);
+            }
+        }
+        Mat dstStripe = dst_.rowRange(rows.begin(), rows.end());
+        cvtColor(srcStripe, dstStripe, CV_HLS2BGR);
+        dstStripe *= 255.0f;
+    }
+
+private:
+    const Mat& src_;
+    Mat& dst_;
+    float intensity_;
+    const int height_;
+
+    BoostColorInvoker& operator=(const BoostColorInvoker&);
+};
+}
+
+int boostColor(InputArray src, OutputArray dst, float intensity)
 {
     Mat srcImg = src.getMat();
 
@@ -10,35 +59,21 @@ int boostColor(cv::InputArray src, cv::OutputArray dst, float intensity)
     CV_Assert(intensity >= 0.0f && intensity <= 1.0f);
 
     int srcImgType = srcImg.type();
-    if (srcImgType!= CV_32FC3)
+    if (srcImgType != CV_32FC3)
     {
+		TIMER_START(to_cv_32f);
         srcImg.convertTo(srcImg, CV_32FC3);
+		TIMER_END(to_cv_32f);
     }
 
-    int height = srcImg.rows;
-    int width = srcImg.cols;
+    dst.create(srcImg.size(), srcImg.type());
+    Mat dstMat = dst.getMat();
+	TIMER_START(parallelBlock);
+    parallel_for(BlockedRange(0, srcImg.rows), BoostColorInvoker(srcImg, dstMat, intensity));
+	TIMER_END(parallelBlock);
+	TIMER_START(toScrType);
+    dstMat.convertTo(dst, srcImgType);
+	TIMER_END(toScrType);
 
-    Mat srcHls(srcImg);
-    srcHls /= 255.0f;
-    cvtColor(srcHls, srcHls, CV_BGR2HLS);
-
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            //increase channel - 3(saturation)
-            float saturation = srcHls.at<Vec3f>(i,j)[2];
-            saturation += intensity;
-            if (saturation > 1.0f)
-            {
-                saturation = 1.0f;
-            }
-            srcHls.at<Vec3f>(i,j)[2] = saturation;
-        }
-    }
-
-    cvtColor(srcHls, dst, CV_HLS2BGR);
-    dst.getMat() *= 255.0f;
-    dst.getMat().convertTo(dst, srcImgType);
     return 0;
 }
