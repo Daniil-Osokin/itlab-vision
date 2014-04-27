533e6e6809f7bc14a748d51299c61de3840767b2
unknown <skrebkov.a@ws-2k-114-06n.ccam.cmc.unn.net>
2014-04-27 11:00:05 +0400

    add utils.hpp

---
+++ photoeffects/include/utils.hpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <algorithm>
+#include <limits>
+#include <math.h>
+
+#define DBL_EPSILON numeric_limits<double>::epsilon()
+
+
+#define TIMER_START(name) int64 t_##name = getTickCount()
+#define TIMER_END(name) printf("TIMER_" #name ":\t%6.2fms\n", \
+            1000.f * ((getTickCount() - t_##name) / getTickFrequency()))
+			
+using namespace std;
+
+struct PerfMetrics
+{
+    double gmean;
+    double gstddev;
+    double median;
+};
+
+static PerfMetrics calculate(vector<double>& times)
+{
+    sort(times.begin(), times.end());
+
+    double gmean = 0;
+    double gstddev = 0;
+    int n = 0;
+    for(vector<double>::const_iterator i = times.begin(); i != times.end(); ++i)
+    {
+        double x = *i;
+        if (x < DBL_EPSILON) continue;
+        double lx = log(x);
+
+        ++n;
+        double delta = lx - gmean;
+        gmean += delta / n;
+        gstddev += delta * (lx - gmean);
+    }
+
+    gstddev = n > 1 ? sqrt(gstddev / (n - 1)) : 0;
+
+    vector<double>::const_iterator start = times.begin();
+    vector<double>::const_iterator end = times.end();
+
+    int offset = 0;
+    if (gstddev > DBL_EPSILON)
+    {
+        double minout = exp(gmean - 3 * gstddev);
+        double maxout = exp(gmean + 3 * gstddev);
+        while(*start < minout) ++start, ++offset;
+        do --end; while(*end > maxout);
+        ++end;
+    }
+
+    n = 0;
+    gmean = 0;
+    gstddev = 0;
+    int m = 0;
+    for(; start != end; ++start)
+    {
+        double x = *start;
+        if (x < DBL_EPSILON) continue;
+        double lx = log(x);
+
+        ++m;
+        double gdelta = lx - gmean;
+        gmean += gdelta / m;
+        gstddev += gdelta * (lx - gmean);
+    }
+
+    PerfMetrics metrics;
+    metrics.gmean = exp(gmean);
+    metrics.gstddev = m > 1 ? sqrt(gstddev / (m - 1)) : 0;
+    metrics.median = n % 2
+            ? (double)times[offset + n / 2]
+            : 0.5 * (times[offset + n / 2] + times[offset + n / 2 - 1]);
+
+    return metrics;
+}
